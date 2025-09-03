59ca6e1f4ff276b76e397938b1f24526115129c8
Farzad <farzadbazargan021@gmail.com>
2025-09-03 19:37:32 +0200

    getting and printing numbers

--- src/Farzad.java
+++ src/Farzad.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Farzad {
     public static void main(String[] args) {
         System.out.println("Farzad");
+        int num1 ;
+        int num2;
+        Scanner sc = new Scanner(System.in);
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        System.out.println(num1);
+        System.out.println(num2);
     }
 }
