1ecf6470f50194c6dcc63eaea53e04fe6a23b5df
taha <taha1060@yahoo.com>
2025-09-07 15:24:04 +0330

    Add scanner and get & sout two number

--- src/TahaKargozar.java
+++ src/TahaKargozar.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class TahaKargozar {
     public static void main(String[] args) {
         System.out.println("TahaKargozar");
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        System.out.println(num1);
+        System.out.println(num2);
     }
 }
